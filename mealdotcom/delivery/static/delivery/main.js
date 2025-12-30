eb35557bfd23a53c718729532bfac0b9ec489ae4
Mogilisetty Ranjith Kumar <ranjithmogili5@gmail.com>
2025-12-30 17:52:24 +0530

    Added signin and signup functionality
    added both signin and signup in the same index.html and gave it functionality using javascript

---
+++ mealdotcom/delivery/static/delivery/main.js
@@ -0,0 +1,9 @@
+function showSignIn() {
+  document.getElementById("signin").style.display = "block";
+  document.getElementById("signup").style.display = "none"
+}
+
+function showSignUp() {
+    document.getElementById("signup").style.display = "block"
+    document.getElementById("signin").style.display = "none";
+}
--- mealdotcom/delivery/views.py
+++ mealdotcom/delivery/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return render(request, "delivery/index.html")
+
+def signup(request):
+  if request.method == 'POST':
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    phone = request.POST.get("phone")
+    return HttpResponse("Sign up Successful")
+
+  else:
+    return HttpResponse("Invalid Response")
